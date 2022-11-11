929baf1ad133fc406453db19374f3314f9c2f609
Masuda-1246 <yaojiezengtian38@gmail.com>
2022-11-11 20:58:51 +0900

    fix migration

--- laravel/database/migrations/2022_11_11_094214_create_comments_table.php
+++ laravel/database/migrations/2022_11_11_094214_create_comments_table.php
@@ -14,7 +14,13 @@ class CreateCommentsTable extends Migration
     public function up()
     {
         Schema::create('comments', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->bigInteger('post_id')->unsigned()->index();
+
+            $table->string('comment')->nullable();
+            $table->foreign('post_id')->references('id')->on('posts')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
